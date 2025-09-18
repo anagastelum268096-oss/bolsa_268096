50dea0e1944469261d1182d60fc0797ccdbfb2f5
anagastelum268096-oss <ana.gastelum268096@potros.itson.edu.mx>
2025-09-18 10:22:37 -0700

    Add files via upload

---
+++ bolsa_268096/src/main/java/com/mycompany/bolsa_268096/Bolsa_268096.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bolsa_268096;
+
+/**
+ *
+ * @author victo
+ */
+public class Bolsa_268096 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
